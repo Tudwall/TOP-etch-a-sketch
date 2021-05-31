d9c5822fa87e597fe5724129515568f1ba94aca9
Pierre Laffaille <pierre.laffaille@outlook.fr>
2021-05-31 16:15:30 +0200

    rework eveverything
    add functions to clear or generate a new grid

--- app.js
+++ app.js
@@ -1,17 +1,9 @@
 // Selectors.
 const container = document.querySelector("#container");
-const resetBtn = document.querySelector("#reset");
+const newSketchBtn = document.querySelector("#new-sketch");
+const clearBtn = document.querySelector("#clear");
 
-// Event Listeners.
-resetBtn.addEventListener("click", () => {
-  let cells = document.querySelectorAll(".cell");
-  cells.forEach((cell) => {
-    cell.style.backgroundColor = "white";
-  });
-});
-
-// Functions.
-
+// Generates a grid.
 for (let i = 0; i < 16 * 16; i++) {
   let cell = document.createElement("div");
   cell.addEventListener("mouseover", () => {
@@ -19,3 +11,40 @@ for (let i = 0; i < 16 * 16; i++) {
   });
   container.appendChild(cell).classList.add("cell");
 }
+
+// Functions.
+function newSketch() {
+  let gridSize = Number(
+    prompt("Choose the new grid size between 1 and 100: ", "")
+  );
+  if (!Number.isInteger(gridSize) || gridSize > 100 || gridSize < 1) {
+    alert("Choose a number between 1 and 100");
+  }
+
+  // Delete the existing grid
+  let cells = document.querySelectorAll(".cell");
+  cells.forEach((cell) => {
+    container.removeChild(cell);
+  });
+
+  // Create the new grid
+  for (let i = 0; i < gridSize * gridSize; i++) {
+    let cell = document.createElement("div");
+    cell.addEventListener("mouseover", () => {
+      cell.style.backgroundColor = "black";
+    });
+    container.style.gridTemplateRows = `repeat(${gridSize}, auto)`;
+    container.style.gridTemplateColumns = `repeat(${gridSize}, auto)`;
+    container.appendChild(cell).classList.add("cell");
+  }
+}
+
+// Event Listeners.
+newSketchBtn.addEventListener("click", () => newSketch());
+
+clearBtn.addEventListener("click", () => {
+  let cells = document.querySelectorAll(".cell");
+  cells.forEach((cell) => {
+    cell.style.backgroundColor = "white";
+  });
+});
