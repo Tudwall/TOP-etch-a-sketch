332760745f82cb1beceb53e15d31ad152843fd5d
Pierre Laffaille <pierre.laffaille@outlook.fr>
2021-05-31 15:19:57 +0200

    add an event listener to clear the grid

--- app.js
+++ app.js
@@ -1,5 +1,16 @@
 // Selectors.
 const container = document.querySelector("#container");
+const resetBtn = document.querySelector("#reset");
+
+// Event Listeners.
+resetBtn.addEventListener("click", () => {
+  let cells = document.querySelectorAll(".cell");
+  cells.forEach((cell) => {
+    cell.style.backgroundColor = "white";
+  });
+});
+
+// Functions.
 
 for (let i = 0; i < 16 * 16; i++) {
   let cell = document.createElement("div");
